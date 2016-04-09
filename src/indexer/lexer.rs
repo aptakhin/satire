6be60c6918eba1fbc7e180a1b14d1e15956771e6
Alexander Ptakhin <me@aptakhin.name>
2016-04-09 22:38:39 +0300

    First refactoring. Indexer separated

---
+++ src/indexer/lexer.rs
@@ -0,0 +1,94 @@
+use std::io::prelude::*;
+
+#[derive(Debug)]
+pub enum LexemType {
+    Unknown,
+    Whitespace,
+    Newline,
+    Token,
+}
+
+pub struct Lexem {
+    pub lexem_type: LexemType,
+    pub content: String,
+    pub start_iter: usize,
+}
+
+pub trait Lexer {
+    fn next(&mut self) -> Option<Lexem>;
+}
+
+pub struct CommonLexer {
+    pub buffer: String,
+    pub read_iter: usize,
+}
+
+impl CommonLexer {
+    pub fn new(buffer: String) -> CommonLexer {
+        CommonLexer {
+            buffer: buffer,
+            read_iter: 0,
+        }
+    }
+}
+
+impl Lexer for CommonLexer {
+    fn next(&mut self) -> Option<Lexem> {
+        let mut read = String::new();
+        let mut lexem_type = LexemType::Unknown;
+        let start_iter = self.read_iter;
+
+        loop {
+            if let Some(ch) = self.buffer.chars().nth(self.read_iter) {
+                let is_whitespace = ch.is_whitespace();
+                match lexem_type {
+                    LexemType::Unknown => {
+                        if ch == '\r' {
+                            lexem_type = LexemType::Newline;
+                            self.read_iter += 2;
+                            read = String::from("\n");
+                            break
+                        } else if ch == '\n' {
+                            lexem_type = LexemType::Newline;
+                        } else if is_whitespace {
+                            lexem_type = LexemType::Whitespace;
+                        }
+                        else {
+                            lexem_type = LexemType::Token;
+                        }
+                    },
+                    LexemType::Token => {
+                        if is_whitespace {
+                            break;
+                        } else if ch == '(' || ch == ')' {
+                            break;
+                        }
+                    },
+                    LexemType::Newline => {
+                        break;
+                    },
+                    LexemType::Whitespace => {
+                        if !is_whitespace {
+                            break;
+                        }
+                    },
+                }
+
+                self.read_iter += 1;
+                read.push_str(&ch.to_string());
+            }
+            else {
+                match lexem_type {
+                    LexemType::Unknown => return None,
+                    _ => break,
+                }
+            }
+        }
+
+        Some(Lexem {
+            lexem_type: lexem_type,
+            content: read,
+            start_iter: start_iter,
+        })
+    }
+}
---
+++ src/indexer/parser.rs
@@ -0,0 +1,233 @@
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::io::BufWriter;
+use std::fs::File;
+use std::collections::HashSet;
+
+use indexer::lexer::Lexem;
+use indexer::lexer::Lexer;
+use indexer::lexer::LexemType;
+use indexer::lexer::CommonLexer;
+
+#[derive(Debug)]
+pub enum ParseState {
+    Wait,
+    KeywordThenName,
+    NameThenCall,
+}
+
+pub struct FileSource {
+    file: String,
+    line: i32,
+    id_iter: usize,
+    lexem_iter: usize,
+}
+
+pub struct Unit {
+    unit_type: String,
+    path: Vec<String>,
+    source: FileSource,
+}
+
+pub struct CommonParser {
+    pub lexems: Vec<Lexem>,
+}
+
+pub trait LL {
+    fn next();
+}
+
+impl LL for CommonParser {
+    fn next()  {
+    }
+}
+
+impl CommonParser {
+    pub fn new() -> CommonParser {
+        CommonParser {
+            lexems: vec![],
+        }
+    }
+
+    pub fn parse(&mut self) {
+
+        let input = File::open("test/src.rs").unwrap();
+
+        let mut reader = BufReader::new(input);
+        let mut buffer = String::new();
+
+        reader.read_to_string(&mut buffer).unwrap();
+
+        let mut lexer = CommonLexer::new(buffer);
+
+        self.lexems.push(Lexem{
+            lexem_type: LexemType::Newline,
+            content: String::from("\n"),
+            start_iter: 0,
+        });
+
+        loop {
+            if let Some(lexem) = lexer.next() {
+                self.lexems.push(lexem);
+            } else {
+                break;
+            }
+        }
+
+        let mut units = vec![];
+        let mut use_units = vec![];
+
+        let mut parse_state = ParseState::Wait;
+
+        let mut line_counter = 0;
+
+        let mut lex_iter = 0;
+
+        let mut use_unit_name = String::new();
+        let mut use_lex_iter = 0;
+        let mut unit_type = String::new();
+
+        loop {
+            let ref cur = &self.lexems[lex_iter];
+            let ref fmt = cur.content;
+
+            println!("S0: {} ({:?})", fmt, parse_state);
+
+
+            match cur.lexem_type {
+                LexemType::Newline => {
+                    line_counter += 1;
+                }
+                _ => {},
+            }
+
+            match parse_state {
+                ParseState::Wait => {
+                    match cur.lexem_type {
+                        LexemType::Token => {
+                            if fmt == "struct" || fmt == "fn" {
+                                unit_type = fmt.to_string();
+                                parse_state = ParseState::KeywordThenName;
+                            } else if fmt == "{" || fmt == "}" {
+                                // Skip this yet
+                            } else { // if like identifier
+                                use_unit_name = fmt.to_string();
+                                use_lex_iter = lex_iter;
+                                parse_state = ParseState::NameThenCall;
+                            }
+                        },
+                        _ => {},
+                    }
+                },
+                ParseState::KeywordThenName => {
+                    match cur.lexem_type {
+                        LexemType::Token => {
+                            let unit_path = vec![cur.content.clone()];
+                            units.push(Unit{
+                                unit_type: unit_type.clone(),
+                                path: unit_path,
+                                source: FileSource{
+                                    file: String::from("src.rs"),
+                                    line: line_counter,
+                                    id_iter: cur.start_iter,
+                                    lexem_iter: lex_iter,
+                                }
+                            });
+                            parse_state = ParseState::Wait;
+                        },
+                        _ => {},
+                    }
+                },
+                ParseState::NameThenCall => {
+                    match cur.lexem_type {
+                        LexemType::Token => {
+                            if fmt == "(" {
+                                let unit_path = vec![use_unit_name.clone()];
+                                use_units.push(Unit{
+                                    unit_type: String::from("fn"),
+                                    path: unit_path,
+                                    source: FileSource{
+                                        file: String::from("src.rs"),
+                                        line: line_counter,
+                                        id_iter: cur.start_iter,
+                                        lexem_iter: use_lex_iter,
+                                    }
+                                });
+                                println!("CC: {}, {}", use_lex_iter, &self.lexems[use_lex_iter].content);
+                            } else {
+                                parse_state = ParseState::Wait;
+                                use_unit_name = String::new();
+                            }
+                        },
+                        _ => {},
+                    }
+                },
+            }
+
+            println!("  -> ({:?})", parse_state);
+
+            if lex_iter == &self.lexems.len() - 1 {
+                break;
+            }
+
+            lex_iter += 1;
+        }
+
+        for unit in &units {
+            println!("U: {}, {}, {}:{}", unit.unit_type, unit.path[0], unit.source.file, unit.source.line);
+        }
+
+        for unit in &use_units {
+            println!("UU: {}, {}, {}:{}", unit.unit_type, unit.path[0], unit.source.file, unit.source.line);
+
+            let ref path = unit.path[0];
+            let res = &units.iter().find(|u| *u.path[0] == path.to_string());
+
+            for u in res {
+                println!("Z: {}", u.path[0]);
+                self.lexems[unit.source.lexem_iter].content = format!("<a href=\"#l{}\">{}</a>", u.source.line, self.lexems[unit.source.lexem_iter].content);
+            }
+        }
+
+        let mut words: HashSet<&str> = HashSet::new();
+        words.insert("struct");
+        words.insert("use");
+        words.insert("fn");
+        words.insert("let");
+        //HashSet<str> = vec!(b"struct", b"main()").iter().collect();
+
+        {
+            //let x = &lexems[0];
+            //println!("F: {} ({:?})", x.content, x.lexem_type);
+        }
+
+
+
+        let output = File::create("test/src.rs.html").unwrap();
+        let mut writer = BufWriter::new(output);
+
+        let mut out = String::new();
+        out.push_str("<pre>");
+
+        line_counter = 1;
+        for lexem in &self.lexems {
+            //println!("G: {} ({:?})", lexem.content, lexem.lexem_type);
+            let ref fmt = &lexem.content;
+
+            match lexem.lexem_type {
+                LexemType::Newline => {
+                    //fmt = format!("{}<a name=\"l{}\">", fmt, line_counter);
+                    line_counter += 1;
+                },
+                _ => {},
+            }
+
+            if words.contains(&fmt[..]) {
+                //fmt = format!("<b>{}</b>", fmt);
+            }
+            out.push_str(&fmt[..]);
+        }
+        out.push_str("</pre>");
+        writer.write(out.as_bytes()).unwrap();
+    }
+}
