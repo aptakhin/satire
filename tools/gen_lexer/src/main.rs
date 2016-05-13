353efff43df43287f0f68710830a5b4243d9c9c9
Alexander Ptakhin <me@aptakhin.name>
2016-05-13 10:29:33 +0300

    Clean gen_lexer

--- tools/gen_lexer/src/main.rs
+++ tools/gen_lexer/src/main.rs
@@ -6,16 +6,13 @@ use std::io::BufRead;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // The first argument is the path that was used to call the program.
-    println!("My path is {}.", args[0]);
-
     if args.len() < 2 {
         println!("Waited lexems file");
         return;
     }
 
-    let mut lexems_file = File::open(&args[1]).unwrap();
-    let mut file = BufReader::new(&lexems_file);
+    let lexems_file = File::open(&args[1]).unwrap();
+    let file = BufReader::new(&lexems_file);
 
     let mut lines = vec![];
 
@@ -38,15 +35,4 @@ fn main() {
     for keyword in &lines {
         print!(" &T_{} |", keyword);
     }
-
-    //panic!("No");
-
-    // The rest of the arguments are the passed command line parameters.
-    // Call the program like this:
-    //   $ ./args arg1 arg2
-    //println!("I got {:?} arguments: {:?}.", args.len() - 1, &args[1..]);
-
-
-    //let mut template = String::new();
-    //template_file.read_to_string(&mut template);
 }
